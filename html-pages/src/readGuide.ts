470cf50d32ae5619b6a1011aaae02a2573f00afd
DTrombett <maxtromb.dt@gmail.com>
2021-08-17 19:12:28 +0200

    Remove html-pages

--- html-pages/src/readGuide.ts
+++
@@ -1,5 +0,0 @@
-import { promises } from "fs";
-import { join } from "path";
-
-export default (fileName: string): Promise<{ html: Buffer; fileName: string }> =>
-	promises.readFile(join(__dirname, "../../guides", fileName)).then((html) => ({ html, fileName }));
